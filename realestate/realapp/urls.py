d7cca4e947393a2c30c0a6f0ed00a80abdc80f70
salmonGowa <gowasalmon@yahoo.com>
2024-02-21 09:07:32 +0300

    base_dir

--- realestate/realapp/urls.py
+++ realestate/realapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .import views
+from realapp import views
 
 urlpatterns = [
     path('handlelogin',views.handlelogin,name="handlelogin"),
-    
+    path('handlesignup',views.handlesignup,name='handlesignup'),
+    path('contact',views.contact,name='contact'),
+    path('about',views.about,name='about'),
+    path('home',views.home,name='home'),
 ]
--- realestate/realapp/views.py
+++ realestate/realapp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def handlelogin(request):
     return render(request,'login.html')
+def handlesignup(request):
+    return render(request,'signup.html')
+def contact(request):
+    return render(request,'contact.html')
+def about(request):
+    return render(request,'about.html')
+def home(request):
+    return render(request,'index.html')
