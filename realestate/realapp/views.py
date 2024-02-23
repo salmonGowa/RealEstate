018f39fec6179ded767483d912ccff44bd4b3f0a
salmonGowa <gowasalmon@yahoo.com>
2024-02-23 21:58:42 +0300

    signup

--- realestate/realapp/views.py
+++ realestate/realapp/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def handlelogin(request):
     
     return render(request,'login.html')
+
+
 def handlesignup(request):
+
+    if request.method=="POST":
+        uname=request.POST['username']
+        password1=request.POST['pass1']
+        confpass=request.POST['pass2']
+        email=request.POST['email']
+
+
+        if password1!=confpass:
+            return HttpResponse("Password Dont Match")
+        
+        user=User.objects.create_user(uname,password1,email)
+        user.save()
+
+
+
     return render(request,'signup.html')
+
+
+
 def contact(request):
     return render(request,'contact.html')
 def about(request):
