535929e51debbc90ff7a143eea932062015878e2
salmonGowa <gowasalmon@yahoo.com>
2024-03-05 18:41:11 +0300

    sqltables

--- realestate/realapp/admin.py
+++ realestate/realapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from realapp.models import Contact
+from realapp.models import Contact,Houses
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(Houses)
--- realestate/realapp/models.py
+++ realestate/realapp/models.py
@@ -6,3 +6,13 @@ class Contact(models.Model):
     email=models.EmailField(max_length=254)
     subject=models.CharField(max_length=50)
     mesage=models.TextField(max_length=150)
+
+
+class Houses(models.Model):
+    house_id=models.AutoField
+   
+    bedrooms=models.IntegerField()
+    location=models.CharField(max_length=50)
+    sizr_sq_feet=models.CharField(max_length=50)
+    
+    
